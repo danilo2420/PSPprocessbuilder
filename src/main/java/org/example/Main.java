37bfbb539cb4154f7ec15d8ebdd89dadb824f598
Danilo <danimoraga1997@gmail.com>
2025-01-30 20:46:47 +0100

    planteamiento

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello rafa!");
+        // Parte 1: Aquí tenemos que leer del archivo y generar la lista
+
+
+
+
+        // Aquí llegamos a la lista
+        ArrayList<String> comandos = new ArrayList<>();
+        comandos.add("calc");
+        comandos.add("notepad.exe");
+
+        // Parte 2: En base a la lista, creamos el menú de usuario
+        System.out.println("Menú:");
+        for (int i = 0; i < comandos.size(); i++) {
+
+        }
+
+
+
+        // Parte 3: Parte de lanzar el proceso
+
+
     }
 }
